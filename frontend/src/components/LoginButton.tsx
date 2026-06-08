788ef021dcd0f2d91373864d1f4cc516ffbef92d
Kamal <kamal.bansal021202@gmail.com>
2026-06-08 12:22:03 +0530

    UI Improves

--- frontend/src/components/LoginButton.tsx
+++ frontend/src/components/LoginButton.tsx
@@ -5,15 +5,26 @@ import {
   signOut,
   useSession,
 } from "next-auth/react";
+import { FcGoogle } from "react-icons/fc";
 
 export default function LoginButton() {
 
   return (
-    <button className="border px-3 py-1 rounded"
+    <button className="flex
+    items-center
+    justify-center
+    gap-2
+    border
+    rounded-lg
+    px-4
+    py-2
+    hover:bg-gray-800
+    transition"
       onClick={() =>
         signIn("google")
       }
     >
+      <FcGoogle size={20} />
       Login With Google
     </button>
   );
